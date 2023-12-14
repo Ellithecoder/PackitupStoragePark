26e9f0dd541a4a1a920dfcf8cc423273f15428c5
Shemar Robinson <shemarrobinson403@gmail.com>
2023-12-14 02:09:25 -0500

    Revert "User storage unit and classes create views"
    This reverts commit 7011f7542ad29b09e4ef1c91dc34f5e16e624871.

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,14 +3,11 @@ using Microsoft.EntityFrameworkCore;
 
 namespace PackitupStoragePark.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<User>
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
-          
-    }
-        public DbSet<User> Users {  get; set; } 
-        public DbSet<StorageUnit> StorageUnits {  get; set; } 
+        }
     }
 }
